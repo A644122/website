b19fac4ec55742f703978cffa325ff62e40a2686
alex <mdafrozalam109@gmail.com>
2025-07-11 09:57:25 +0530

    icon changes footer components

--- src/components/Footers.jsx
+++ src/components/Footers.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Web from "../assets/Logo.jpg"; // Adjust the path as necessary
+import Webthreelogo from "../assets/webthreelogo.jpg"; // Adjust the path as necessary
 const Footers = () => {
   return (
     <footer className="bg-gray-900 text-white py-10 px-6 md:px-12 lg:px-20">
@@ -8,7 +8,7 @@ const Footers = () => {
         <div>
           {/* Logo */}
           <div className="mb-4">
-            <img src={Web} alt="Logo" className="h-10 w-auto" />
+            <img src={Webthreelogo} alt="Logo" className="h-10 w-auto" />
           </div>
 
           {/* Paragraph */}
